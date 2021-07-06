1c434463b48e8455903d5fe3b8312969456a627e
omooba1 <iyboyidavid@gmail.com>
2021-07-06 11:02:31 +0100

    Note

---
+++ app/src/main/java/com/iyoboyi/notes/adapters/NoteAdapter.kt
@@ -0,0 +1,28 @@
+package com.iyoboyi.notes.adapters
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.iyoboyi.notes.databinding.NoteItemBinding
+import com.iyoboyi.notes.models.Note
+
+class NoteAdapter(val notes: List<Note>) : RecyclerView.Adapter<NoteAdapter.NoteViewHolder>()  {
+
+    class NoteViewHolder(val binding: NoteItemBinding): RecyclerView.ViewHolder(binding.root){
+        fun bind(note: Note){
+            binding.idView.text = note.id.toString()
+            binding.titleView.text = note.title
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
