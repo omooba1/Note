2c348e6f077ce537d3b153c6feb4a4a7061787fc
omooba1 <iyoboyidavid@gmail.com>
2021-07-06 14:36:19 +0100

    Note New

--- app/src/main/java/com/iyoboyi/notes/MainActivity.kt
+++ app/src/main/java/com/iyoboyi/notes/MainActivity.kt
@@ -2,6 +2,9 @@ package com.iyoboyi.notes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.room.Room
+import com.iyoboyi.notes.adapters.NoteAdapter
 import com.iyoboyi.notes.databinding.ActivityMainBinding
 import com.iyoboyi.notes.models.Note
 import com.iyoboyi.notes.models.NoteDatabase
@@ -10,6 +13,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var database: NoteDatabase
+    private lateinit var noteAdapter: NoteAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,11 +21,32 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        database = Room.databaseBuilder(
+            applicationContext,
+            NoteDatabase::class.java,
+            "notes_database"
+        ).allowMainThreadQueries().build()
+
+        noteAdapter = NoteAdapter(database.noteDao().getAllNotes())
+        binding.notesRecycler.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = noteAdapter
+        }
+
+        binding.saveButton.setOnClickListener {
+            val title = binding.titleName.text.toString()
+            val content = binding.contentName.text.toString()
+
+            saveNote(title, content)
+        }
+
 
     }
 
     private fun saveNote(title: String, content: String) {
-    val note = Note(id = 0, title, content)
+        val note = Note(id = 0, title, content)
+        database.noteDao().addNote(note)
+        noteAdapter.notifyDataSetChanged()
 
     }
 }
--- app/src/main/java/com/iyoboyi/notes/adapters/NoteAdapter.kt
+++ app/src/main/java/com/iyoboyi/notes/adapters/NoteAdapter.kt
@@ -1,21 +1,23 @@
 package com.iyoboyi.notes.adapters
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.iyoboyi.notes.databinding.NoteItemBinding
 import com.iyoboyi.notes.models.Note
 
-class NoteAdapter(val notes: List<Note>) : RecyclerView.Adapter<NoteAdapter.NoteViewHolder>()  {
+class NoteAdapter(private val notes: List<Note>) : RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
 
-    class NoteViewHolder(val binding: NoteItemBinding): RecyclerView.ViewHolder(binding.root){
-        fun bind(note: Note){
+    class NoteViewHolder(private val binding: NoteItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(note: Note) {
             binding.idView.text = note.id.toString()
             binding.titleView.text = note.title
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
-        TODO("Not yet implemented")
+        var binding = NoteItemBinding.inflate(LayoutInflater.from(parent.context))
+        return NoteViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
